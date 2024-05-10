6dd7bb9901a9571dc2bf2db08f77ac0cfd85b959
Toby-exe <tobyyyw@gmail.com>
2024-05-10 00:40:35 -0600

    Refactored SIRD_plot such that it can run standalone (tested and working) or as part of the shiny app (not testes since shiny app seems to be broken atm)

--- R/SIRD_plot.R
+++ R/SIRD_plot.R
@@ -1,121 +1,158 @@
-sird_plot <- function() {
-  expr <- quote({
-    sird_equations <- function(time, variables, parameters) {
-      S <- variables[1]
-      I <- variables[2]
-      R <- variables[3]
-      D <- variables[4]
-      N <- variables[5]
-      q <- variables[6]
-      dS <- (input$muBirth * N) - (input$muDeath * S) - (input$betaSIRD * ((S * I) / (N^q)))
-      dI <- (input$betaSIRD * ((S * I) / (N^q))) - (input$gammaSIRD * I) - (input$deltaSIRD * I) - (input$muDeath * I)
-      dR <- (input$gammaSIRD * I) - (input$muDeath * R)
-      dD <- (input$deltaSIRD * I)
-      dN <- dS + dI + dR
-      list(c(dS, dI, dR, dD, dN, q))
-    }
+library(deSolve)
+library(ggplot2)
 
-    sird_values <- reactive({
-      req(input$timesteps, input$betaSIRD, input$gammaSIRD, input$deltaSIRD, input$muBirth, input$muDeath)
+equationsSIRD <- function(time, variables, parameters) {
+    S <- variables[1]
+    I <- variables[2]
+    R <- variables[3]
+    D <- variables[4]
+    N <- variables[5]
+    q <- variables[6]
+    dS <- (parameters["muB"] * N) - (parameters["muD"] * S) - (parameters["beta"] * ((S * I) / (N^q)))
+    dI <- (parameters["beta"] * ((S * I) / (N^q))) - (parameters["gamma"] * I) - (parameters["delta"] * I) - (parameters["muD"] * I)
+    dR <- (parameters["gamma"] * I) - (parameters["muD"] * R)
+    dD <- (parameters["delta"] * I)
+    dN <- dS + dI + dR
+    list(c(dS, dI, dR, dD, dN, q))
+}
 
-      validate(
-        need(input$betaSIRD >= 0, "Enter a valid value for parameters"),
-        need(input$gammaSIRD >= 0, "Enter a valid value for parameters"),
-        need(input$deltaSIRD >= 0, "Enter a valid value for parameters"),
-        need(input$populationSIRD > 0, "Enter a valid value for parameters"),
-        need(input$susceptibleSIRD > 0, "Enter a valid value for parameters"),
-        need(input$infectedSIRD >= 0, "Enter a valid value for parameters"),
-        need(input$recoveredSIRD >= 0, "Enter a valid value for parameters"),
-        need(input$deadSIRD >= 0, "Enter a valid value for parameters"),
-        need(input$timesteps > 0, "Timesteps must be greater than 0."),
-        need(input$populationSIRD == (input$susceptibleSIRD + input$infectedSIRD + input$recoveredSIRD), "Number of persons living in all epidemic compartments must sum to the Population Count (N)")
-      )
+defaultParams <- list(
+    beta = 0.001,
+    gamma = 0.1,
+    delta = 0.05,
+    muB = 0,
+    muD = 0,
+    population = 500,
+    susceptible = 499,
+    infected = 1,
+    recovered = 0,
+    dead = 0,
+    timesteps = 50,
+    q = 0
+)
 
-      ode(
+# For standalone usage
+# TODO: find out if there's a better way to check if the script is being 
+# run standalone or as part of a shiny app
+solveSIRD <- function() {
+    params <- defaultParams
+
+    source <- if (!shiny::isRunning()) params else input
+
+    timesteps <- source$timesteps
+    beta <- source$beta
+    gamma <- source$gamma
+    delta <- source$delta
+    muB <- source$muB
+    muD <- source$muD
+    population <- source$population
+    susceptible <- source$susceptible
+    infected <- source$infected
+    recovered <- source$recovered
+    dead <- source$dead
+    q <- source$q
+
+    # Using lsoda
+    solnsSIRD <- lsoda(
         y = c(
-          S = input$susceptibleSIRD,
-          I = input$infectedSIRD,
-          R = input$recoveredSIRD,
-          D = input$deadSIRD,
-          N = input$populationSIRD,
-          q = as.integer(input$qValue)
+            S = susceptible,
+            I = infected,
+            R = recovered,
+            D = dead,
+            N = population,
+            q = q
         ),
-        times = seq(0, input$timesteps, by = 1),
-        func = sird_equations,
+        times = seq(0, timesteps, by = 1),
+        func = equationsSIRD,
         parms = c(
-          beta = input$betaSIRD,
-          gamma = input$gammaSIRD,
-          delta = input$deltaSIRD,
-          muB = input$muBirth,
-          muD = input$muDeath
+            beta = beta,
+            gamma = gamma,
+            delta = delta,
+            muB = muB,
+            muD = muD
         )
-      )
-    })
+    )
 
-    output$plotSIRD <- renderPlot({
-      input$go
-      isolate({
-        val <- as.data.frame(sird_values())
-        ggplot(val, aes(x = time)) +
-          theme(
-            axis.line = element_line(color = "black"), axis.text = element_text(size = 14),
+    as.data.frame(solnsSIRD)
+}
+
+plotSIRD <- function() {
+    valuesSIRD <- solveSIRD()
+    ggplot(valuesSIRD, aes(x = time)) +
+        theme(
+            axis.line = element_line(color = "black"),
+            axis.text = element_text(size = 14),
             axis.title.x = element_text(size = 16, face = "bold"),
             axis.title.y = element_text(size = 16, face = "bold")
-          ) +
-          ggtitle("SIRD Epidemic Model") +
-          theme(plot.title = element_text(size = 22, face = "bold")) +
-          theme(legend.position = "bottom") +
-          ylab("Number of People") +
-          xlab("Time") +
-          scale_x_continuous(expand = c(0, 0)) +
-          scale_y_continuous(expand = c(0, 0)) +
-          geom_line(aes(y = S, color = "Blue"), linewidth = 1.5) +
-          geom_line(aes(y = I, color = "Red"), linewidth = 1.5) +
-          geom_line(aes(y = R, color = "Green"), linewidth = 1.5) +
-          geom_line(aes(y = D, color = "Orange"), linewidth = 1.5) +
-          scale_color_identity(
+        ) +
+        ggtitle("SIRD Epidemic Model") +
+        theme(plot.title = element_text(size = 22, face = "bold")) +
+        theme(legend.position = "bottom") +
+        ylab("Number of People") +
+        xlab("Time") +
+        scale_x_continuous(expand = c(0, 0)) +
+        scale_y_continuous(expand = c(0, 0)) +
+        geom_line(aes(y = S, color = "Blue"), linewidth = 1.5) +
+        geom_line(aes(y = I, color = "Red"), linewidth = 1.5) +
+        geom_line(aes(y = R, color = "Green"), linewidth = 1.5) +
+        geom_line(aes(y = D, color = "Orange"), linewidth = 1.5) +
+        scale_color_identity(
             name = "SIRD", breaks = c("Blue", "Red", "Green", "Orange"),
             labels = c("Susceptible", "Infected", "Recovered", "Dead"), guide = "legend"
-          )
-      })
-    })
+        )
+}
 
-    output$SIRDPhasePlane <- renderPlot({
-      input$go
-      isolate({
-        val <- as.data.frame(sird_values())
-        ggplot(val, aes(x = S)) +
-          geom_line(aes(y = I, color = "Blue"), linewidth = 1.5) +
-          theme(
-            axis.line = element_line(color = "black"), axis.text = element_text(size = 14),
+
+plotPhasePlaneSIRD <- function() {
+    valuesSIRD <- solveSIRD()
+    ggplot(valuesSIRD, aes(x = S)) +
+        geom_line(aes(y = I, color = "Blue"), linewidth = 1.5) +
+        theme(
+            axis.line = element_line(color = "black"),
+            axis.text = element_text(size = 14),
             axis.title.x = element_text(size = 16, face = "bold"),
             axis.title.y = element_text(size = 16, face = "bold")
-          ) +
-          theme(legend.position = "bottom") +
-          ggtitle("SI Phase Plane") +
-          theme(plot.title = element_text(size = 22, face = "bold")) +
-          ylab("Infected (I)") +
-          scale_x_continuous(expand = c(0, 0)) +
-          scale_y_continuous(expand = c(0, 0)) +
-          xlab("Susceptible (S)") +
-          scale_color_identity(
-            breaks = "Blue",
-            labels = "Susceptible"
-          )
-      })
+        ) +
+        theme(legend.position = "bottom") +
+        ggtitle("SI Phase Plane") +
+        theme(plot.title = element_text(size = 22, face = "bold")) +
+        ylab("Infected (I)") +
+        scale_x_continuous(expand = c(0, 0)) +
+        scale_y_continuous(expand = c(0, 0)) +
+        xlab("Susceptible (S)") +
+        scale_color_identity(
+            name = "SIRD", breaks = c("Blue", "Red", "Green", "Orange"),
+            labels = c("Susceptible", "Infected", "Recovered", "Dead"), guide = "legend"
+        )
+}
+
+# no longer need to do shiny::isRunning() check since this function is only called
+# from server.R
+solveAndRenderSIRD <- function() {
+    expr <- quote({
+        output$plotSIRD <- renderPlot({
+            plotSIRD()
+        })
+        output$SIRDPhasePlane <- renderPlot({
+            plotPhasePlaneSIRD()
+        })
+        output$tableSIRD <- renderTable({
+            return(solveSIRD()[-c(6)])
+        })
     })
 
-    output$tableSIRD <- renderTable({
-      input$go
-      isolate({
-        val <- as.data.frame(sird_values())
-        val <- val[-c(7)]
-        return(val)
-      })
-    })
-  })
+    eval.parent(expr)
+}
 
-  ## Evaluate the above qouted expression in the parent frame (the calling
-  ## environment).
-  eval.parent(expr)
+# For standalone usage
+# TODO: find out if there's a better way to check if the script is being 
+# run standalone or as part of a shiny app
+if (!shiny::isRunning()) {
+    plot <- plotSIRD()
+    print(plot)
+    ggsave("sird_plot.png", plot)
+
+    phasePlanePlot <- plotPhasePlaneSIRD()
+    print(phasePlanePlot)
+    ggsave("sird_phase_plane_plot.png", phasePlanePlot)
 }
