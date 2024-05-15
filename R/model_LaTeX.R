e494e162439fa30bcfe097ee2efb4046a1ff487f
Khanh Le <lekhanhsone@gmail.com>
2024-05-15 11:51:28 -0600

    New update-There is an error

--- R/model_LaTeX.R
+++ R/model_LaTeX.R
@@ -1,64 +1,166 @@
 SIR_LaTeX <- function(mu) {
   switch(mu + 1, 
     helpText(
-      r"(Susceptible $$\frac{dS}{dt} = - \beta \frac{ S I}{N^q}$$)",
-      r"(Infectious $$\frac{dI}{dt} = \frac{\beta S I}{N^q} - \gamma {I} $$)",
-      r"(Recovered $$\frac{dR}{dt} = \gamma {I} $$)",
-      r"(Reproductive ratio $$R_0 =  \frac{\beta}{\gamma} S(0)^q$$)",
-      r"(q-Value $$\begin{equation} x = \begin{cases} 1 & \text{, } frequency − dependent \\\\ 0 & \text{, } density − dependent \end{cases}\end{equation} $$)"
+      HTML(paste0(
+        '<style>',
+        '  .equation-container {',
+        '    display: flex;',
+        '    justify-content: flex-start;',
+        '  }',
+        '  .equation-block {',
+        '    margin: 0;',
+        '    padding: 0;',
+        '  }',
+        '</style>',
+        '<div class="equation-container">',
+        '<blockquote class="equation-block">',
+        '\\begin{align*}',
+        '    &', r"(\frac{dS}{dt} = - \beta \frac{ S I}{N^q}\\)", 
+        '    &', r"(\frac{dI}{dt} = \frac{\beta S I}{N^q} - \gamma {I})", ' \\\\',
+        '    &', r"(\frac{dR}{dt} = \gamma {I})", ' \\\\',
+        '    &', r"(R_0 =  \frac{\beta}{\gamma} S(0)^q)", ' \\\\',
+        '    &', r"(\begin{cases} 1 & \text{, frequency-dependent} \\\\ 0 & \text{, density-dependent} \end{cases})", ' \\\\',
+        '\\end{align*}',
+        '</blockquote>',
+        '</div>'
+      ))
     ),
     helpText(
-      r"(Susceptible $$\frac{dS}{dt} =\mu_B N - \mu_D S - \beta \frac{ S I}{N^q}$$)",
-      r"(Infectious $$\frac{dI}{dt} = \frac{\beta S I}{N^q} - \gamma {I} - \mu_D I$$)",
-      r"(Recovered $$\frac{dR}{dt} = \gamma {I} - \mu_D R $$)",
-      r"(Reproductive ratio $$R_0 =  \frac{\beta}{\gamma} S(0)^q$$)",
-      r"(q-Value $$\begin{equation} x = \begin{cases} 1 & \text{, } frequency − dependent \\\\ 0 & \text{, } density − dependent \end{cases}\end{equation} $$)"
+      HTML(paste0(
+        '<style>',
+        '  .equation-container {',
+        '    display: flex;',
+        '    justify-content: flex-start;',
+        '  }',
+        '  .equation-block {',
+        '    margin: 0;',
+        '    padding: 0;',
+        '  }',
+        '</style>',
+        '<div class="equation-container">',
+        '<blockquote class="equation-block">',
+        '\\begin{align*}',
+        '    &', r"(\frac{dS}{dt} = \mu_B N - \mu_D S - \beta \frac{ S I}{N^q})", ' \\\\',
+        '    &', r"(\frac{dI}{dt} = \frac{\beta S I}{N^q} - \gamma I - \mu_D {I})", ' \\\\',
+        '    &', r"(\frac{dR}{dt} = \gamma I - \mu_D {R})", ' \\\\',
+        '    &', r"(R_0 =  \frac{\beta}{\gamma} S(0)^q)", ' \\\\',
+        '    &', r"(\begin{cases} 1 & \text{, frequency-dependent} \\\\ 0 & \text{, density-dependent} \end{cases})", ' \\\\',
+        '\\end{align*}',
+        '</blockquote>',
+        '</div>'
+      ))
     )
   )
 }
 
-## Khanh TODO: Please write the switch statement with the appropriate LaTeX, in
-## raw strings as in the other functions in this file, for this model. NOTE: if,
-## and only if, the function needs the value of the mu parameter should you give
-## the function the formal parameter; if needed, then make the function call in
-## server.R pass input$muValue also.
 SIRS_LaTeX <- function(mu) {
   switch(mu + 1, 
          helpText(
-           r"(Susceptible $$\frac{dS}{dt} = - \beta \frac{ S I}{N} + \xi{R}$$)",
-           r"(Infectious $$\frac{dI}{dt} = \frac{\beta S I}{N} - \gamma {I}$$)",
-           r"(Recovered $$\frac{dR}{dt} = \gamma {I} - \xi{R} $$)",
-           r"(Reproductive ratio $$R_0 =  \frac{\beta}{\gamma} S(0)^q$$)", #need to double check 
-           r"(q-Value $$\begin{equation} x = \begin{cases} 1 & \text{, } frequency − dependent \\\\ 0 & \text{, } density − dependent \end{cases}\end{equation} $$)"
-           ),
+           HTML(paste0(
+             '<style>',
+             '  .equation-container {',
+             '    display: flex;',
+             '    justify-content: flex-start;',
+             '  }',
+             '  .equation-block {',
+             '    margin: 0;',
+             '    padding: 0;',
+             '  }',
+             '</style>',
+             '<div class="equation-container">',
+             '<blockquote class="equation-block">',
+             '\\begin{align*}',
+             '    &', r"(\frac{dS}{dt} = - \beta \frac{ S I}{N} + \xi{R})", ' \\\\',
+             '    &', r"(\frac{dI}{dt} = \frac{\beta S I}{N} - \gamma {I})", ' \\\\',
+             '    &', r"(\frac{dR}{dt} = \gamma {I} - \xi{R})", ' \\\\',
+             '    &', r"(R_0 =  \frac{\beta}{\gamma} S(0)^q)", ' \\\\',
+             '    &', r"(\begin{cases} 1 & \text{, frequency-dependent} \\\\ 0 & \text{, density-dependent} \end{cases})", ' \\\\',
+             '\\end{align*}',
+             '</blockquote>',
+             '</div>'
+           ))
+         ),
          helpText(
-           # want to make sure it is \nu{S}
-           r"(Susceptible $$\frac{dS}{dt} = \mu{N}- \beta \frac{ S I}{N} + \xi{R} - \nu{S}$$)", 
-           r"(Infectious $$\frac{dI}{dt} = \frac{\beta S I}{N} - \gamma {I} - \nu{I}$$)",
-           r"(Recovered $$\frac{dR}{dt} = \gamma {I} - \xi{R} - \nu{R}$$)",
-           r"(Reproductive ratio $$R_0 =  \frac{\beta}{\gamma} S(0)^q$$)", #need to double check 
-           r"(q-Value $$\begin{equation} x = \begin{cases} 1 & \text{, } frequency − dependent \\\\ 0 & \text{, } density − dependent \end{cases}\end{equation} $$)"
-           )
+           HTML(paste0(
+             '<style>',
+             '  .equation-container {',
+             '    display: flex;',
+             '    justify-content: flex-start;',
+             '  }',
+             '  .equation-block {',
+             '    margin: 0;',
+             '    padding: 0;',
+             '  }',
+             '</style>',
+             '<div class="equation-container">',
+             '<blockquote class="equation-block">',
+             '\\begin{align*}',
+             '    &', r"(\frac{dS}{dt} = \mu{N}- \beta \frac{ S I}{N} + \xi{R} - \nu{S})", ' \\\\',
+             '    &', r"(\frac{dI}{dt} = \frac{\beta S I}{N} - \gamma {I} - \nu{I})", ' \\\\',
+             '    &', r"(\frac{dR}{dt} = \gamma {I} - \xi{R} - \nu{R})", ' \\\\',
+             '    &', r"(R_0 =  \frac{\beta}{\gamma} S(0)^q)", ' \\\\',
+             '    &', r"(\begin{cases} 1 & \text{, frequency-dependent} \\\\ 0 & \text{, density-dependent} \end{cases})", ' \\\\',
+             '\\end{align*}',
+             '</blockquote>',
+             '</div>'
+           ))
          )
+  )
 }
 
 SIRD_LaTeX <- function(mu) {
   switch(mu + 1,
     helpText(
-      r"(Susceptible $$\frac{dS}{dt} = - \beta \frac{ S I}{N^q}$$)",
-      r"(Infectious $$\frac{dI}{dt} = \frac{\beta S I}{N^q} - \gamma {I} - \delta {I} $$)",
-      r"(Recovered $$\frac{dR}{dt} = \gamma {I} $$)",
-      r"(Dead $$\frac{dD}{dt} = \delta {I} $$)",
-      r"(Reproductive ratio $$R_0 =  \frac{\beta}{\gamma + \delta} S(0)^q$$)",
-      r"(q-Value $$\begin{equation} x = \begin{cases} 1 & \text{, } frequency − dependent \\\\ 0 & \text{, } density − dependent \end{cases}\end{equation} $$)"
+      HTML(paste0(
+        '<style>',
+        '  .equation-container {',
+        '    display: flex;',
+        '    justify-content: flex-start;',
+        '  }',
+        '  .equation-block {',
+        '    margin: 0;',
+        '    padding: 0;',
+        '  }',
+        '</style>',
+        '<div class="equation-container">',
+        '<blockquote class="equation-block">',
+        '\\begin{align*}',
+        '    &', r"(\frac{dS}{dt} = - \beta \frac{ S I}{N^q})", ' \\\\',
+        '    &', r"(\frac{dI}{dt} = \frac{\beta S I}{N^q} - \gamma {I} - \delta {I} )", ' \\\\',
+        '    &', r"(\frac{dR}{dt} = \gamma {I} )", ' \\\\',
+        '    &', r"(\frac{dD}{dt} = \delta {I})", ' \\\\',
+        '    &', r"(R_0 =  \frac{\beta}{\gamma} S(0)^q)", ' \\\\',
+        '    &', r"(\begin{cases} 1 & \text{, frequency-dependent} \\\\ 0 & \text{, density-dependent} \end{cases})", ' \\\\',
+        '\\end{align*}',
+        '</blockquote>',
+        '</div>'
+      ))
     ),
     helpText(
-      r"(Susceptible $$\frac{dS}{dt} =\mu_B N - \mu_D S - \beta \frac{ S I}{N^q}$$)",
-      r"(Infectious $$\frac{dI}{dt} = \frac{\beta S I}{N^q} - \gamma {I} - \delta {I} - \mu_D I$$)",
-      r"(Recovered $$\frac{dR}{dt} = \gamma {I} - \mu_D R $$)",
-      r"(Dead $$\frac{dD}{dt} = \delta {I} $$)",
-      r"(Reproductive ratio $$R_0 =  \frac{\beta}{\gamma + \delta} S(0)^q$$)",
-      r"(q-Value $$\begin{equation} x = \begin{cases} 1 & \text{, } frequency − dependent \\\\ 0 & \text{, } density − dependent \end{cases}\end{equation} $$)"
+      HTML(paste0(
+        '<style>',
+        '  .equation-container {',
+        '    display: flex;',
+        '    justify-content: flex-start;',
+        '  }',
+        '  .equation-block {',
+        '    margin: 0;',
+        '    padding: 0;',
+        '  }',
+        '</style>',
+        '<div class="equation-container">',
+        '<blockquote class="equation-block">',
+        '\\begin{align*}',
+        '    &', r"(\frac{dS}{dt} =\mu_B {N} - \mu_D {S} - \beta \frac{ S I}{N^q})", ' \\\\',
+        '    &', r"(\frac{dI}{dt} = \frac{\beta S I}{N^q} - \gamma {I} - \delta {I} - \mu_D {I})", ' \\\\',
+        '    &', r"(\frac{dR}{dt} = \gamma {I} - \mu_D {R} )", ' \\\\',
+        '    &', r"(\frac{dD}{dt} = \delta {I})", ' \\\\',
+        '    &', r"(R_0 =  \frac{\beta}{\gamma} S(0)^q)", ' \\\\',
+        '    &', r"(\begin{cases} 1 & \text{, frequency-dependent} \\\\ 0 & \text{, density-dependent} \end{cases})", ' \\\\',
+        '\\end{align*}',
+        '</blockquote>',
+        '</div>'
+      ))
     )
   )
 }
@@ -66,20 +168,56 @@ SIRD_LaTeX <- function(mu) {
 SEIR_LaTeX <- function(mu) {
   switch(mu + 1,
     helpText(
-      r"(Susceptible $$\frac{dS}{dt} = - \beta \frac{ S I}{N^q}$$)",
-      r"(Exposed $$\frac{dE}{dt} = \beta \frac{ S I}{N^q} - \gamma {E} $$)",
-      r"(Infectious $$\frac{dI}{dt} = \gamma {E} - \sigma {I} $$)",
-      r"(Recovered $$\frac{dR}{dt} = \sigma {I} $$)",
-      r"(Reproductive ratio $$R_0 =  \frac{\beta}{\gamma}$$)",
-      r"(q-Value $$\begin{equation} x = \begin{cases} 1 & \text{, } frequency − dependent \\\\ 0 & \text{, } density − dependent \end{cases}\end{equation} $$)"
+      HTML(paste0(
+        '<style>',
+        '  .equation-container {',
+        '    display: flex;',
+        '    justify-content: flex-start;',
+        '  }',
+        '  .equation-block {',
+        '    margin: 0;',
+        '    padding: 0;',
+        '  }',
+        '</style>',
+        '<div class="equation-container">',
+        '<blockquote class="equation-block">',
+        '\\begin{align*}',
+        '    &', r"(\frac{dS}{dt} = - \beta \frac{ S I}{N^q})", ' \\\\',
+        '    &', r"(\frac{dE}{dt} = \beta \frac{ S I}{N^q} - \gamma {E})", ' \\\\',
+        '    &', r"(\frac{dI}{dt} = \gamma {E} - \sigma {I})", ' \\\\',
+        '    &', r"(\frac{dR}{dt} = \sigma {I})", ' \\\\',
+        '    &', r"(R_0 =  \frac{\beta}{\gamma} S(0)^q)", ' \\\\',
+        '    &', r"(\begin{cases} 1 & \text{, frequency-dependent} \\\\ 0 & \text{, density-dependent} \end{cases})", ' \\\\',
+        '\\end{align*}',
+        '</blockquote>',
+        '</div>'
+      ))
     ),
     helpText(
-      r"(Susceptible $$\frac{dS}{dt} =\mu_B N - \mu_D S - \beta \frac{ S I}{N^q}$$)",
-      r"(Exposed $$\frac{dE}{dt} = \beta \frac{ S I}{N^q} - \gamma {E} - \mu_D {E}$$)",
-      r"(Infectious $$\frac{dI}{dt} = \gamma E - \sigma I - \mu_D I $$)",
-      r"(Recovered $$\frac{dR}{dt} = \sigma I - \mu_D R $$)",
-      r"(Reproductive ratio $$R_0 =  \frac{\beta}{\gamma}$$)",
-     r"(q-Value $$\begin{equation} x = \begin{cases} 1 & \text{, } frequency − dependent \\\\ 0 & \text{, } density − dependent \end{cases}\end{equation} $$)"
+      HTML(paste0(
+        '<style>',
+        '  .equation-container {',
+        '    display: flex;',
+        '    justify-content: flex-start;',
+        '  }',
+        '  .equation-block {',
+        '    margin: 0;',
+        '    padding: 0;',
+        '  }',
+        '</style>',
+        '<div class="equation-container">',
+        '<blockquote class="equation-block">',
+        '\\begin{align*}',
+        '    &', r"(\frac{dS}{dt} = \mu_B {N} - \mu_D {S} - \beta \frac{ S I}{N^q})", ' \\\\',
+        '    &', r"(\frac{dE}{dt} = \beta \frac{ S I}{N^q} - \gamma {E} - - \mu_D {E})", ' \\\\',
+        '    &', r"(\frac{dI}{dt} = \gamma {E} - \sigma {I} - \mu_D {I})", ' \\\\',
+        '    &', r"(\frac{dR}{dt} = \sigma {I}) - \mu_D {R} )", ' \\\\',
+        '    &', r"(R_0 =  \frac{\beta}{\gamma} S(0)^q)", ' \\\\',
+        '    &', r"(\begin{cases} 1 & \text{, frequency-dependent} \\\\ 0 & \text{, density-dependent} \end{cases})", ' \\\\',
+        '\\end{align*}',
+        '</blockquote>',
+        '</div>'
+      ))
     )
   )
 }
@@ -87,22 +225,58 @@ SEIR_LaTeX <- function(mu) {
 SEIRD_LaTeX <- function(mu) {
   switch(mu + 1,
     helpText(
-      r"(Susceptible $$\frac{dS}{dt} = - \beta \frac{ S I}{N^q}$$)",
-      r"(Exposed $$\frac{dE}{dt} = \beta \frac{ S I}{N^q} - \gamma E $$)",
-      r"(Infectious $$\frac{dI}{dt} = \gamma E - \sigma I - \delta I $$)",
-      r"(Recovered $$\frac{dR}{dt} = \sigma I $$)",
-      r"(Dead $$ \frac{dD}{dt} = \delta I $$)",
-      r"(Reproductive ratio $$R_0 =  \frac{\beta}{\gamma}$$)",
-      r"(q-Value $$\begin{equation} x = \begin{cases} 1 & \text{, } frequency − dependent \\\\ 0 & \text{, } density − dependent \end{cases}\end{equation} $$)"
+      HTML(paste0(
+        '<style>',
+        '  .equation-container {',
+        '    display: flex;',
+        '    justify-content: flex-start;',
+        '  }',
+        '  .equation-block {',
+        '    margin: 0;',
+        '    padding: 0;',
+        '  }',
+        '</style>',
+        '<div class="equation-container">',
+        '<blockquote class="equation-block">',
+        '\\begin{align*}',
+        '    &', r"(\frac{dS}{dt} = - \beta \frac{ S I}{N^q})", ' \\\\',
+        '    &', r"(\frac{dE}{dt} = \beta \frac{ S I}{N^q} - \gamma {E})", ' \\\\',
+        '    &', r"(\frac{dI}{dt} = \gamma {E} - \sigma {I} - \delta {I} )", ' \\\\',
+        '    &', r"(\frac{dR}{dt} = \sigma {I})", ' \\\\',
+        '    &', r"(\frac{dD}{dt} = \delta {I})", ' \\\\',
+        '    &', r"(R_0 =  \frac{\beta}{\gamma} S(0)^q)", ' \\\\',
+        '    &', r"(\begin{cases} 1 & \text{, frequency-dependent} \\\\ 0 & \text{, density-dependent} \end{cases})", ' \\\\',
+        '\\end{align*}',
+        '</blockquote>',
+        '</div>'
+      ))
     ),
     helpText(
-      r"(Susceptible $$\frac{dS}{dt} =\mu_B N - \mu_D S - \beta \frac{ S I}{N^q}$$)",
-      r"(Exposed $$\frac{dE}{dt} = \beta \frac{ S I}{N^q} - \gamma E - \mu_D E$$)",
-      r"(Infectious $$\frac{dI}{dt} = \gamma E - \sigma I -  \delta I- \mu_D I $$)",
-      r"(Recovered $$\frac{dR}{dt} = \sigma I - \mu_D R $$)",
-      r"(Dead $$ \frac{dD}{dt} = \delta I $$)",
-      r"(Reproductive ratio $$R_0 =  \frac{\beta}{\gamma}$$)",
-      r"(q-Value $$\begin{equation} x = \begin{cases} 1 & \text{, } frequency − dependent \\\\ 0 & \text{, } density − dependent \end{cases}\end{equation} $$)"
+      HTML(paste0(
+        '<style>',
+        '  .equation-container {',
+        '    display: flex;',
+        '    justify-content: flex-start;',
+        '  }',
+        '  .equation-block {',
+        '    margin: 0;',
+        '    padding: 0;',
+        '  }',
+        '</style>',
+        '<div class="equation-container">',
+        '<blockquote class="equation-block">',
+        '\\begin{align*}',
+        '    &', r"(\frac{dS}{dt} = \mu_B {N} - \mu_D {S} - \beta \frac{ S I}{N^q})", ' \\\\',
+        '    &', r"(\frac{dE}{dt} = \beta \frac{ S I}{N^q} - \gamma {E} - - \mu_D {E})", ' \\\\',
+        '    &', r"(\frac{dI}{dt} = \gamma {E} - \sigma {I} -  \delta {I} - \mu_D {I})", ' \\\\',
+        '    &', r"(\frac{dR}{dt} = \sigma {I}) - \mu_D {R} )", ' \\\\',
+        '    &', r"(\frac{dD}{dt} = \delta {I})", ' \\\\',
+        '    &', r"(R_0 =  \frac{\beta}{\gamma} S(0)^q)", ' \\\\',
+        '    &', r"(\begin{cases} 1 & \text{, frequency-dependent} \\\\ 0 & \text{, density-dependent} \end{cases})", ' \\\\',
+        '\\end{align*}',
+        '</blockquote>',
+        '</div>'
+      ))
     )
   )
 }
