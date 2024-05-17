588f4c4de517a016e78a1780ccaa572cf244daa7
Khanh Le <lekhanhsone@gmail.com>
2024-05-17 11:54:45 -0600

    New update - Reformatting model_Latex.R code

--- R/mathemtical_equations.R
+++ R/mathemtical_equations.R
@@ -1,108 +1,128 @@
-#SIR PMA with no vital dynamic
-SIR_PMA_nonVD <- c(r"(    &\frac{dS}{dt} = - \beta { S I}\\)",
-                   r"(    &\frac{dI}{dt} = {\beta S I} - \gamma {I}\\)")
-#SIR PMA with vital dynamic
-SIR_PMA_VD <- c(r"(    &\frac{dS}{dt} = \mu_B N - \mu_D S - \beta { S I}\\)", 
-                r"(    &\frac{dI}{dt} = {\beta S I} - \gamma I - \mu_D {I}\\)")
-#SIR TMA with no vital dynamic
-SIR_TMA_nonVD <- c(r"(    &\frac{dS}{dt} = - \beta \frac{ S I}{N}\\)", 
-                   r"(    &\frac{dI}{dt} = \frac{\beta S I}{N} - \gamma {I}\\)")
-#SIR TMA with no vital dynamic
-SIR_TMA_VD <- c(r"(    &\frac{dS}{dt} = \mu_B N - \mu_D S - \beta \frac{ S I}{N}\\)", 
-                r"(    &\frac{dI}{dt} = \frac{\beta S I}{N} - \gamma I - \mu_D {I}\\)")
+#Susceptible ode 
+Susceptible <- r"(    &\frac{dS}{dt} = )"
+#Infection ode
+Infection <- r"(    &\frac{dI}{dt} = )"
+#Recover ode 
+Recover <- r"(    &\frac{dR}{dt} = )"
+#Exposure ode 
+Exposure <- r"(    &\frac{dE}{dt} = )"
+#Dead ode 
+Dead <- r"(    &\frac{dD}{dt} = )"
 
-# SIR model with no vital dynamic
-SIR_nonVD_equation <- c(r"(    &\frac{dR}{dt} = \gamma {I}\\)", 
-                        r"(    &R_0 =  \frac{\beta}{\gamma} S(0)^q\\)") #Need to verify the equation 
 
-# SIR model with vital dynamic
-SIR_VD_equation <- c(r"(    &\frac{dR}{dt} = \gamma I - \mu_D {R}\\)", 
-                     r"(    &R_0 =  \frac{\beta}{\gamma} S(0)^q\\)") #Need to verify the equation 
+getForceOfInfection <- function(qValue)
+{
+  if (qValue == 0){forceOfInfection = r"( \beta { S I})"} else {forceOfInfection = r"( \beta \frac{ S I}{N})" }
+  return(forceOfInfection)
+}
 
-#-----------------------------------------------------------------------------------------------------
-#SIRS PMA with no vital dynamic
-SIRS_PMA_nonVD <- c(r"(    &\frac{dS}{dt} = - \beta{ S I} + \xi{R}\\)",
-                    r"(    &\frac{dI}{dt} = {\beta S I} - \gamma {I}\\)")
-#SIRS PMA with vital dynamic
-SIRS_PMA_VD <- c(r"(    &\frac{dS}{dt} = \mu{N}- \beta { S I} + \xi{R} - \nu{S}\\)",  
-                r"(    &\frac{dI}{dt} = {\beta S I} - \gamma {I} - \nu{I}\\)")
-#SIRS TMA with no vital dynamic
-SIRS_TMA_nonVD <- c(r"(    &\frac{dS}{dt} = - \beta \frac{ S I}{N} + \xi{R}\\)",
-                   r"(    &\frac{dI}{dt} = \frac{\beta S I}{N} - \gamma {I}\\)")
-#SIR TMA with no vital dynamic
-SIRS_TMA_VD <- c(r"(    &\frac{dS}{dt} = \mu{N}- \beta \frac{ S I}{N} + \xi{R} - \nu{S}\\)",  
-                r"(    &\frac{dI}{dt} = \frac{\beta S I}{N} - \gamma {I} - \nu{I}\\)")
-# SIRS with no vital dynamic
-SIRS_nonVD_equation <- c(r"(    &\frac{dR}{dt} = \gamma {I} - \xi{R}\\)", 
-                         r"(    &R_0 =  \frac{\beta}{\gamma} S(0)^q\\)") #Need to verify the equation 
-# SIRS with vital dynamic
-SIRS_VD_equation <- c(r"(    &\frac{dR}{dt} = \gamma {I} - \xi{R} - \nu{R}\\)", 
-                       r"(    &R_0 =  \frac{\beta}{\gamma} S(0)^q\\)") #Need to verify the equation 
+SIR <- function(mu,forceOfInfection)
+{
+  if (!mu)
+  {
+    S = paste0(Susceptible, "- ", forceOfInfection, r"(\\)")
+    I = paste0(Infection, forceOfInfection, r"(- \gamma {I}\\)")
+    R = paste0(Recover, r"(\gamma {I}\\)")
+  } else 
+  {
+    S = paste0(Susceptible, r"(\mu_B N - \mu_D S -)", forceOfInfection)
+    I = paste0(Infection, forceOfInfection, r"(- \gamma I - \mu_D {I}\\)")
+    R = paste0(Recover, r"(\gamma {I} - \mu_D {R}\\)")
+  }
+  equation <- c(S,I,R)
+  return(equation)
+}
 
-#-----------------------------------------------------------------------------------------------------
-#SIRD PMA with no vital dynamic
-SIRD_PMA_nonVD <- c(r"(    &\frac{dS}{dt} = - \beta { S I}\\)", 
-                    r"(    &\frac{dI}{dt} = {\beta S I} - \gamma {I} - \delta {I}\\)")
-#SIRD PMA with vital dynamic
-SIRD_PMA_VD <- c(r"(    &\frac{dS}{dt} =\mu_B {N} - \mu_D {S} - \beta { S I}\\)",  
-                 r"(    &\frac{dI}{dt} = {\beta S I} - \gamma {I} - \delta {I} - \mu_D {I}\\)")
-#SIRS TMA with no vital dynamic
-SIRD_TMA_nonVD <- c( r"(    &\frac{dS}{dt} = - \beta \frac{ S I}{N^q}\\)", 
-                     r"(    &\frac{dI}{dt} = \frac{\beta S I}{N^q} - \gamma {I} - \delta {I}\\)")
-#SIR TMA with no vital dynamic
-SIRD_TMA_VD <- c(r"(    &\frac{dS}{dt} =\mu_B {N} - \mu_D {S} - \beta \frac{ S I}{N^q}\\)",  
-                 r"(    &\frac{dI}{dt} = \frac{\beta S I}{N^q} - \gamma {I} - \delta {I} - \mu_D {I}\\)")
-# SIRD model with no vital dynamic
-SIRD_nonVD_equation <- c(r"(    &\frac{dR}{dt} = \gamma {I}\\)", 
-                         r"(    &\frac{dD}{dt} = \delta {I}\\)", 
-                         r"(    &R_0 =  \frac{\beta}{\gamma} S(0)^q\\)") #Need to verify the equation 
-# SIRD model with vital dynamic
-SIRD_VD_equation <- c(r"(    &\frac{dR}{dt} = \gamma {I} - \mu_D {R}\\)",
-                      r"(    &\frac{dD}{dt} = \delta {I}\\)", 
-                      r"(    &R_0 =  \frac{\beta}{\gamma} S(0)^q\\)") #Need to verify the equation 
 
-#-----------------------------------------------------------------------------------------------------
-#SEIR PMA with no vital dynamic
-SEIR_PMA_nonVD <- c(r"(    &\frac{dS}{dt} = - \beta { S I}\\)", 
-                    r"(    &\frac{dE}{dt} = \beta { S I} - \gamma {E}\\)")
-#SEIR PMA with vital dynamic
-SEIR_PMA_VD <- c(r"(    &\frac{dS}{dt} = \mu_B {N} - \mu_D {S} - \beta { S I}\\)", 
-                 r"(    &\frac{dE}{dt} = \beta { S I} - \gamma {E} - \mu_D {E}\\)")
-#SEIR TMA with no vital dynamic
-SEIR_TMA_nonVD <- c(r"(    &\frac{dS}{dt} = - \beta \frac{ S I}{N^q}\\)", 
-                    r"(    &\frac{dE}{dt} = \beta \frac{ S I}{N^q} - \gamma {E}\\)")
-#SEIR TMA with no vital dynamic
-SEIR_TMA_VD <- c(r"(    &\frac{dS}{dt} = \mu_B {N} - \mu_D {S} - \beta \frac{ S I}{N^q}\\)", 
-                 r"(    &\frac{dE}{dt} = \beta \frac{ S I}{N^q} - \gamma {E} - \mu_D {E}\\)")
-# SEIR model with no vital dynamic
-SEIR_nonVD_equation <- c(r"(    &\frac{dI}{dt} = \gamma {E} - \sigma {I}\\)", 
-                         r"(    &\frac{dR}{dt} = \sigma {I}\\)", 
-                         r"(    &R_0 =  \frac{\beta}{\gamma} S(0)^q\\)") #Need to verify the equation 
-# SEIR model with vital dynamic
-SEIR_VD_equation <- c(r"(    &\frac{dI}{dt} = \gamma {E} - \sigma {I} - \mu_D {I}\\)", 
-                      r"(    &\frac{dR}{dt} = \sigma {I}) - \mu_D {R}\\)",  
-                      r"(    &R_0 =  \frac{\beta}{\gamma} S(0)^q\\)") #Need to verify the equation 
+SIRS <- function(mu,forceOfInfection)
+{
+  if (!mu)
+  {
+    S = paste0(Susceptible, "- ", forceOfInfection, r"(+ \xi{R}\\)")
+    I = paste0(Infection, forceOfInfection, r"(- \gamma {I}\\)")
+    R = paste0(Recover, r"(\gamma {I} - \xi{R}\\)" )
+  } else 
+  {
+    S = paste0(Susceptible, r"( \mu{N} -)", forceOfInfection, r"(+ \xi{R} - \nu{S}\\)")
+    I = paste0(Infection, forceOfInfection, r"(- \gamma {I} - \nu{I}\\)" )
+    R = paste0(Recover, r"(\gamma {I} - \xi{R} - \nu{R}\\)")
+  }
+  equation <- c(S,I,R)
+  return(equation)
+}
 
-#-----------------------------------------------------------------------------------------------------
-#SEIRD PMA with no vital dynamic
-SEIRD_PMA_nonVD <- c(r"(    &\frac{dS}{dt} = - \beta { S I}\\)", 
-                     r"(    &\frac{dE}{dt} = \beta { S I} - \gamma {E}\\)")
-#SEIRD PMA with vital dynamic
-SEIRD_PMA_VD <- c(r"(    &\frac{dS}{dt} = \mu_B {N} - \mu_D {S} - \beta { S I}\\)",
-                  r"(    &\frac{dE}{dt} = \beta { S I} - \gamma {E} - \mu_D {E}\\)")
-#SEIRD TMA with no vital dynamic
-SEIRD_TMA_nonVD <- c(r"(    &\frac{dS}{dt} = - \beta \frac{ S I}{N^q}\\)", 
-                     r"(    &\frac{dE}{dt} = \beta \frac{ S I}{N^q} - \gamma {E}\\)")
-#SEIRD TMA with no vital dynamic
-SEIRD_TMA_VD <- c(r"(    &\frac{dS}{dt} = \mu_B {N} - \mu_D {S} - \beta \frac{ S I}{N^q}\\)",
-                  r"(    &\frac{dE}{dt} = \beta \frac{ S I}{N^q} - \gamma {E} - \mu_D {E}\\)")
-# SEIRD model with no vital dynamic
-SEIRD_nonVD_equation <- c(r"(    &\frac{dI}{dt} = \gamma {E} - \sigma {I} - \delta {I}\\)",
-                          r"(    &\frac{dR}{dt} = \sigma {I}\\)",  
-                          r"(    &\frac{dD}{dt} = \delta {I}\\)",
-                          r"(    &R_0 =  \frac{\beta}{\gamma} S(0)^q\\)") #Need to verify the equation 
-# SEIRD model with vital dynamic
-SEIRD_VD_equation <- c(r"(    &\frac{dI}{dt} = \gamma {E} - \sigma {I} -  \delta {I} - \mu_D {I}\\)", 
-                       r"(    &\frac{dR}{dt} = \sigma {I}) - \mu_D {R}\\)", 
-                       r"(    &\frac{dD}{dt} = \delta {I}\\)", 
-                       r"(    &R_0 =  \frac{\beta}{\gamma} S(0)^q\\)") #Need to verify the equation 
+SIRD <- function(mu,forceOfInfection)
+{
+  if (!mu)
+  {
+    S <- paste0(Susceptible, "- ", forceOfInfection)
+    I <- paste0(Infection, forceOfInfection, r"(- \gamma {I} - \delta {I}\\)")
+    R <- paste0(Recover, r"(\gamma {I}\\)")
+  }else 
+  {
+    S <- paste0(Susceptible, r"(\mu_B {N} - \mu_D {S} - )", forceOfInfection)
+    I <- paste0(Infection, forceOfInfection, r"(- \gamma {I} - \delta {I} - \mu_D {I}\\)" )
+    R <- paste0(Recover, r"(\gamma {I} - \mu_D {R}\\)")
+  }
+  D <- paste0(Dead, r"(\delta {I})" )
+  equation <- c(S,I,R,D)
+  return(equation)
+}
+
+SEIR <- function(mu,forceOfInfection)
+{
+  if (!mu)
+  {
+    S <- paste0(Susceptible, "- ", forceOfInfection)
+    E <- paste0(Exposure, forceOfInfection, r"(- \gamma {E}\\)")
+    I <- paste0(Infection, r"(\gamma {E} - \sigma {I}\\)" )
+    R <- paste0(Recover, r"(\sigma {I}\\)")
+  } else 
+  {
+    S <- paste0(Susceptible, r"(\mu_B {N} - \mu_D {S} -)", forceOfInfection)
+    E <- paste0(Exposure, forceOfInfection, r"(- \gamma {E} - \mu_D {E}\\)")
+    I <- paste0(Infection, r"(\gamma {E} - \sigma {I} - \mu_D {I}\\)")
+    R <- paste0(Recover, r"(\sigma {I} - \mu_D {R}\\)")
+  }
+  equation <- c(S,E,I,R)
+  return(equation)
+}
+
+SEIRD <- function(mu,forceOfInfection)
+{
+  if (!mu)
+  {
+    S <- paste0(Susceptible, "- ", forceOfInfection)
+    E <- paste0(Exposure, forceOfInfection, r"(- \gamma {E}\\)")
+    I <- paste0(Infection, r"(\gamma {E} - \sigma {I} - \delta {I}\\)" )
+    R <- paste0(Recover, r"(\sigma {I}\\)")
+  } else 
+  {
+    S <- paste0(Susceptible, r"(\mu_B {N} - \mu_D {S} -)", forceOfInfection)
+    E <- paste0(Exposure, forceOfInfection, r"(- \gamma {E} - \mu_D {E}\\)")
+    I <- paste0(Infection, r"(\gamma {E} - \sigma {I} -  \delta {I} - \mu_D {I}\\)")
+    R <- paste0(Recover, r"(\sigma {I} - \mu_D {R}\\)")
+  }
+  D <- paste0(Dead, r"(\delta {I})" )
+  equation <- c(S,E,I,R,D)
+  return(equation)
+}
+
+SEIRS <- function(mu,forceOfInfection)
+{
+  if (!mu)
+  {
+    S <- paste0(Susceptible, "- ", forceOfInfection, r"(+ \xi {R})")
+    E <- paste0(Exposure, forceOfInfection, r"(- \gamma {E}\\)")
+    I <- paste0(Infection, r"(\gamma {E} - \sigma {I}\\)" )
+    R <- paste0(Recover, r"(\sigma {I} - \xi{R}\\)")
+  } else 
+  {
+    S <- paste0(Susceptible, r"(\mu_B {N} - \mu_D {S} -)", forceOfInfection)
+    E <- paste0(Exposure, forceOfInfection, r"(- \gamma {E} - \mu_D {E}\\)")
+    I <- paste0(Infection, r"(\gamma {E} - \sigma {I} -  \delta {I} - \mu_D {I}\\)")
+    R <- paste0(Recover, r"(\sigma {I} - \mu_D {R}\\)")
+  }
+  equation <- c(S,E,I,R)
+  return(equation)
+}
--- R/model_LaTeX.R
+++ R/model_LaTeX.R
@@ -27,59 +27,33 @@ generate_latex <- function(equations) {
 }
 
 SIR_LaTeX <- function(mu,qValue) {
-  if(!mu)
-  {
-    if (qValue==0){helpText(generate_latex(c(SIR_PMA_nonVD,SIR_nonVD_equation)))}
-    else {helpText(generate_latex(c(SIR_TMA_nonVD,SIR_nonVD_equation)))}
-  } else 
-  {
-    if (qValue==0){helpText(generate_latex(c(SIR_PMA_VD,SIR_VD_equation)))}
-    else {helpText(generate_latex(c(SIR_TMA_VD,SIR_VD_equation)))}
-  }
+  forceOfInfection <- getForceOfInfection(qValue)
+  helpText(generate_latex(SIR(mu,forceOfInfection)))
 }
 
 SIRS_LaTeX <- function(mu,qValue) {
-  if(!mu)
-  {
-    if (qValue==0){helpText(generate_latex(c(SIRS_PMA_nonVD,SIRS_nonVD_equation)))}
-    else {helpText(generate_latex(c(SIRS_TMA_nonVD,SIRS_nonVD_equation)))}
-  } else {
-    if (qValue==0){helpText(generate_latex(c(SIRS_PMA_VD,SIRS_VD_equation)))}
-    else {helpText(generate_latex(c(SIRS_TMA_VD,SIRS_VD_equation)))}
-  }
+  forceOfInfection <- getForceOfInfection(qValue)
+  helpText(generate_latex(SIRS(mu,forceOfInfection)))
 }
 
 SIRD_LaTeX <- function(mu,qValue) {
-  if(!mu)
-  {
-    if (qValue==0){helpText(generate_latex(c(SIRD_PMA_nonVD,SIRD_nonVD_equation)))}
-    else {helpText(generate_latex(c(SIRD_TMA_nonVD,SIRD_nonVD_equation)))}
-  } else {
-    if (qValue==0){helpText(generate_latex(c(SIRD_PMA_VD,SIRD_VD_equation)))}
-    else {helpText(generate_latex(c(SIRD_TMA_VD,SIRD_VD_equation)))}
-  }
+  forceOfInfection <- getForceOfInfection(qValue)
+  helpText(generate_latex(SIRD(mu,forceOfInfection)))
 }
 
 SEIR_LaTeX <- function(mu,qValue) {
-  if(!mu)
-  {
-    if (qValue==0){helpText(generate_latex(c(SEIR_PMA_nonVD,SEIR_nonVD_equation)))}
-    else {helpText(generate_latex(c(SEIR_TMA_nonVD,SEIR_nonVD_equation)))}
-  } else {
-    if (qValue==0){helpText(generate_latex(c(SEIR_PMA_VD,SEIR_VD_equation)))}
-    else {helpText(generate_latex(c(SEIR_TMA_VD,SEIR_VD_equation)))}
-  }
+  forceOfInfection <- getForceOfInfection(qValue)
+  helpText(generate_latex(SEIR(mu,forceOfInfection)))
+}
+
+SEIRS_LaTeX <- function(mu,qValue) {
+  forceOfInfection <- getForceOfInfection(qValue)
+  helpText(generate_latex(SEIRS(mu,forceOfInfection)))
 }
 
 SEIRD_LaTeX <- function(mu,qValue) {
-  if(!mu)
-  {
-    if (qValue==0){helpText(generate_latex(c(SEIRD_PMA_nonVD,SEIRD_nonVD_equation)))}
-    else {helpText(generate_latex(c(SEIRD_TMA_nonVD,SEIRD_nonVD_equation)))}
-  } else {
-    if (qValue==0){helpText(generate_latex(c(SEIRD_PMA_VD,SEIRD_VD_equation)))}
-    else {helpText(generate_latex(c(SEIRD_TMA_VD,SEIRD_VD_equation)))}
-  }
+  forceOfInfection <- getForceOfInfection(qValue)
+  helpText(generate_latex(SEIRD(mu,forceOfInfection)))
 }
 
 ## FIXME: is this working or not?
