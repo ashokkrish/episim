25566009131a0e71a4115986cefacb22fcea6c99
Bryce Carson <bcars268@mtroyal.ca>
2024-05-23 21:22:03 -0600

    Fix slow loading time

--- global.R
+++ global.R
@@ -9,25 +9,29 @@ library(shinyjs)
 library(shinyvalidate)
 library(shinyWidgets)
 library(xlsx) # FIXME: this is slow and gross.
+library(rlang)
 library(R.utils)
+library(readxl)
 
-## NEXT: switch to using the rpojroot package when preparing this Shiny
-## application for deployment, because the here package is a lightweight wrapper
-## around that. There may be shortcomings to the switch, or there may be cons to
-## not switching. Close examination of the final production environment being
-## prepared by T.W. will determine what we finally use. For now, during
-## interactive development of the application, here is a perfect solution.
 here::i_am("global.R")
 
-## FIXME: switch to a readr or super fast library, not this old, slow stuff.
-defaultInputValues <- read.xlsx(here("data/defaultInputValues.xlsx"),
-  sheetIndex = 1
-)
-## FIXME: switch to a readr or super fast library, not this old, slow stuff.
+## DONE: do not modify the column types given below; they are current as of
+## 2024-05-23. They should only change if the ordering of columns changes.
+columnTypes <- c("text", # Model name
+                 ## FIXME: See the related fixme in the filter() call in the
+                 ## defaults() reactive expresison. Using logicals in any way
+                 ## seems to bork filtering for no obvious reason.
+                 ## Column types :: numeric -> logical, or logical -> numeric ::
+                 ## stochastic, vitalDynamics, trueMassAction
+                 ## "logical", "logical", "logical",
+                 character(19))
+columnTypes[2:20] <- "numeric"
+defaultInputValues <- read_xlsx(here("data/defaultInputValues.xlsx"),
+                                col_types = columnTypes)
+
 ## TODO: implement the necessary UI and server logic to have the following be
 ## meaningful to the application.
-## publishedInputValues <- read.xlsx(here("data/defaultInputValues.xlsx"),
-##                                 sheetIndex = 2)
+## publishedInputValues <- read_xlsx(here("data/defaultInputValues.xlsx"), 2)
 
 ## Load the minified JavaScript to extend the modelSelect widget functionality.
 modelSelectJavaScript <- read_file(here("www/modelSelect.min.js"))
