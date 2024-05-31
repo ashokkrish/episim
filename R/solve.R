8b89ef7fea55d523b7f23c3224dfe0b28eb4b28e
Bryce Carson <bcars268@mtroyal.ca>
2024-05-31 00:24:22 -0600

    Obsolte separate solvers for SI-type and SEI-type models.
    Realized the cause of #91 was the (unnecessary and optional) inclusion of
    trueMassAction in the list returned from the differentialEquations function (fix
    
    Made the differentialEquations function local to the unified solver: exposuRe.
    
    Changed the way lists and vectors are joined together in exposuRe, ensuring:
    vectors remain vectors and are not coerced to lists when undesirable; lists are
    appended to correctly; coersion is allowed to take place when necessary.
    
    Added protections for some edge cases that I did not anticipate in the first
    edition of the unified solver.
    
    Resolved an ephemeral regression (having no tracking issue) which caused the
    number of returned derivatives to be one greater than the number of state
    variables when running SEIRS or SIRS models. This was caused by an assignment of
    the modified form dS (when loss of immunity is enabled) to a component of the
    compartment local variable, which was erroneous.
    
    Included more commentary and named vector elements and list components
    descriptively, which aided in debugging this edition and making it a viable
    product.
    
    Reworded some existing commentary.

--- R/solve.R
+++ R/solve.R
@@ -1,55 +1,3 @@
-## MAYBE FIXME: the differential equations don't use the time variable; this may
-## be mathematically or scientifically inaccurate; assess this fact, then
-## resolve this as needed.
-differentialEquations <- function(time, variables, parameters, ...,
-                                  trueMassAction = FALSE) {
-  ## NOTE: is coersion to a list really necessary for each? Can't named vectors
-  ## be appended, or even concatenated with c(), then converted to a list for
-  ## use with with()?
-  with(append(as.list(variables),
-              as.list(parameters)),
-  {
-    trueMassAction <- get("trueMassAction") ## FIXME: I don't like writing this here, but it seems necessary.
-
-    ## Regardless of exposure
-    betaSIN <- beta * ((S * I) / N^trueMassAction)
-    dS <- c(muB * N, -betaSIN, -muD * S)
-    dD <- delta * I # Always zero if delta == 0.
-
-    ## Define the IR compartments with respect to the E compartment.
-    if(sigma == 0) {
-      ## Without exposure, there is no E compartment. γ has its meaning as in
-      ## an SI-type model.
-      compartments <- list(dI = c((betaSIN), -(gamma * I), -(muD * I)),
-                           dR = c((gamma * I), -(muD * R)))
-      printf("Length of compartments: %s", length(compartments))
-    } else {
-      ## With exposure, there is is an E compartment. γ has its meaning as in
-      ## an SEI-type model.
-      compartments <- list(dE = c((betaSIN), -(gamma * E), -(muD * E)),
-                           dI = c((gamma * E), -(sigma * I), -(muD * I)),
-                           dR = c((sigma * I), -(muD * R)))
-      printf("Length of compartments: %s", length(compartments))
-    }
-
-    ## Enable loss of immunity if xi is non-zero.
-    if (xi != 0) {
-      whomAreSusceptibleAgain <- xi * R
-      compartments$dS <- append(compartments$dS, whomAreSusceptibleAgain)
-      compartments$dR <- append(compartments$dR, -whomAreSusceptibleAgain)
-    }
-
-    ## Enable fatality if delta is non-zero.
-    if (delta != 0) compartments$dI <- append(compartments$dI, -dD)
-
-    ## NOTE: dD is already equal to its sum, and is not part of the
-    ## calculation of dN. dN is non-zero if muB != muD.
-    compartmentSums <- lapply(append(dS, compartments), sum)
-    dN <- Reduce(`+`, compartmentSums)
-    list(as.numeric(list(dN, compartmentSums, dD)), trueMassAction)
-  })
-}
-
 ## Package namesake: hexagon logo should have Canadian symbology, perhaps with
 ## "exposure" meaning exposure to the elements and the risk of frostbite.
 ## Therefore, a potential logo could feature an Inuit person wearing ᐃᓪᒑᒃ (or
@@ -81,19 +29,71 @@ exposuRe <-
   if(delta == 0) stopifnot(dead == 0)
   if(sigma == 0) {
     stopifnot(exposed == 0)
+    stopifnot(infected > 0)
     ## Ensure lsoda is called with the correct number of initial state variables
     ## to match the number of differential equations it will return based on the
     ## variables and parameters given to it. I.E., don't call lsoda with the
     ## exposed compartment if the model won't use the compartment.
-    print(variables)
     variables <- variables[!(names(variables) %in% c("E"))]
-    print(variables)
   }
 
+  differentialEquations <-
+    function(time, variables, parameters, ...,
+             trueMassAction = FALSE) {
+      ## NOTE: is coersion to a list really necessary for each? Can't named
+      ## vectors be appended, or even concatenated with c(), then converted to a
+      ## list for use with with()?
+      with(append(as.list(variables),
+                  as.list(parameters)),
+      {
+        ## FIXME: I don't like writing this here, but it seems necessary.
+        trueMassAction <- get("trueMassAction")
+
+        ## Regardless of exposure
+        betaSIN <- beta * ((S * I) / N^trueMassAction)
+        dS <- c(muB * N, -betaSIN, -muD * S)
+        dD <- delta * I # Always zero if delta == 0.
+
+        ## Define the IR compartments with respect to the E compartment.
+        if(sigma == 0) {
+          ## Without exposure, there is no E compartment. γ has its meaning as
+          ## in an SI-type model.
+          compartments <- list(dI = c((betaSIN), -(gamma * I), -(muD * I)),
+                               dR = c((gamma * I), -(muD * R)))
+        } else {
+          ## With exposure, there is is an E compartment. γ has its meaning as
+          ## in an SEI-type model.
+          compartments <- list(dE = c((betaSIN), -(gamma * E), -(muD * E)),
+                               dI = c((gamma * E), -(sigma * I), -(muD * I)),
+                               dR = c((sigma * I), -(muD * R)))
+        }
+
+        ## Enable loss of immunity if xi is non-zero. FIXME: a second S compartment is being added.
+        if (xi != 0) {
+          whomAreSusceptibleAgain <- xi * R
+          dS <- c(dS, whomAreSusceptibleAgain)
+          compartments$dR <- c(compartments$dR, -whomAreSusceptibleAgain)
+        }
+
+        ## Enable fatality if delta is non-zero.
+        if (delta != 0) compartments$dI <- c(compartments$dI, -dD)
+
+        ## NOTE: dD is already equal to its sum, and is not part of the
+        ## calculation of dN. dN is non-zero if muB != muD. Name dS again; it is
+        ## helpful while debugging and printing the compartmentSums.
+        compartmentSums <- lapply(append(list(dS = dS), compartments), sum)
+        dN <- Reduce(`+`, compartmentSums)
+        derivatives <- as.numeric(c(dN, compartmentSums, dD))
+        names(derivatives) <- c("dN", names(compartmentSums), "dD")
+        list(derivatives = derivatives)
+      })
+    }
+
   lsoda(variables,
-        ## TODO: FIXME: these need to be updated with reading on lsoda.
+        ## TODO FIXME: these times need to be updated after reading on lsoda.
         seq(1, length = timesteps, by = increment),
-        equationsSusceptibleInfected,
+        ## MAYBE FIXME: the differential equations don't use the time variable.
+        differentialEquations,
         parameters,
         trueMassAction = as.numeric(trueMassAction)) |>
     as.data.frame()
