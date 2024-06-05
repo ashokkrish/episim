b48f726dbba2727205840f1d5b35de905d48c5a2
Bryce Carson <bcars268@mtroyal.ca>
2024-06-04 18:05:49 -0600

    In-line the definition of documentReadyPainter.R
    It was only called once, so its function body really did belong here.

--- server.R
+++ server.R
@@ -1,5 +1,24 @@
 server <- function(input, output, session) {
-  documentReadyPainter() # Restyle some elements with JavaScript.
+  ## Restyle some elements with JavaScript.
+  runjs(r"[$(document).ready($('#modelSelect + button').removeClass('btn-light'))]")
+  r"($(document)
+     .ready($('div:has(> #modelSelect)')
+            .css({
+            'border':'var(--bs-border-width) solid #8D959E',
+            'border-radius':'var(--bs-border-radius)',
+            'transition':'border-color 0.15s ease-in-out,box - shadow 0.15 s ease - in - out '
+            })
+     )
+    )" |>
+     gsub(pattern = r"(\n\s*)", replacement = "") |>
+     runjs()
+
+  ## NOTE: Disable the stochastic radio button and the sidebar to toggle
+  ## between sub-apps, respectively. TODO: When these are implemented the
+  ## respective line can be removed.
+  ## disable(selector = "#stochastic > div:nth-child(2) > label:nth-child(2) > input:nth-child(1)")
+  disable(selector = "button.collapse-toggle") # sidebar button
+  runjs(r"--($('button.collapse-toggle').hide())--") # sidebar button
 
   globalValidator <- addRuleListToValidator(
     InputValidator$new(),
