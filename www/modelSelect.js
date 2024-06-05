92dfe11cb41b60c6aa8241aa23bcc281c5c3c294
Bryce Carson <bcars268@mtroyal.ca>
2024-06-04 18:09:14 -0600

    Remove a JavaScript console log, and update commentary.
    The console log is not necessary anymore, and the commentary has been updated to
    read like prose and a listing.

--- www/modelSelect.js
+++ www/modelSelect.js
@@ -1,11 +1,12 @@
 $(document).ready($(document).on('shiny:inputchanged', function(event) {
-    // If the model selection widget was interacted with,
+    // If the model selection widget was interacted with, and it't not empty...
     if (event.name === 'modelSelect' && !([''].includes(event.value))) {
+        // modify the CSS applied to modelSelect.
         $('div:has(> #modelSelect-label)').css('margin-bottom', '1rem');
+
         // Protect the following expressions from running until the visualchange
         // event is fired, i.e. the conditionalPanels have changed.
         $(document).on('shiny:visualchange', function(event) {
-            console.log("Shiny visual change!");
             var hiddenInputsArray = [];
             $('.shiny-bound-input:hidden').each(function(index) {
                 hiddenInputsArray.push($(this).attr('id'));
@@ -13,6 +14,7 @@ $(document).ready($(document).on('shiny:inputchanged', function(event) {
             Shiny.setInputValue('hiddenInputs', hiddenInputsArray);
         });
     } else if (event.name === 'modelSelect') {
+        // Modify the CSS applied to modelSelect.
         $('div:has(> #modelSelect-label)').css('margin-bottom', 0);
     }
 }));
